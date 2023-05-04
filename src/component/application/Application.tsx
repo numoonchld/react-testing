8752b5703d1a2bbd686f0fb8c1bfb8dc2cfd22ba
numoonchld <raghavendra.saralaya@gmail.com>
2023-05-04 23:04:41 +0530

    getbyalttext example

--- src/component/application/Application.tsx
+++ src/component/application/Application.tsx
@@ -6,6 +6,7 @@ const Application = () => {
       <h1>Job Application Form</h1>
       <h2>Section 1</h2>
       <p>All fields are mandatory</p>
+      <img src='https://via.placeholder.com/150' alt='a person with a laptop' />
       <form>
         <div>
           <label htmlFor='name'>Name</label>
