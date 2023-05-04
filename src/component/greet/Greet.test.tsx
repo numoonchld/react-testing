f8c6bc3f68a1b8e028b61d3a6a920960fa367e18
numoonchld <raghavendra.saralaya@gmail.com>
2023-05-04 16:24:26 +0530

    grouping tests with describe

--- src/component/greet/Greet.test.tsx
+++ src/component/greet/Greet.test.tsx
@@ -1,17 +1,21 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./Greet"
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
+describe("Greet", () => {
 
-    const text = screen.getByText("Hello")
-    expect(text).toBeInTheDocument()
+    test('Greet renders correctly', () => {
+        render(<Greet />)
     
-})
+        const text = screen.getByText("Hello")
+        expect(text).toBeInTheDocument()
+        
+    })
+    
+    test("Greet renders with a name", () => {
+        render(<Greet name="Ancilla" />)
+    
+        const greetingText = screen.getByText('Hello Ancilla')
+        expect(greetingText).toBeInTheDocument()
+    })
 
-test("Greet renders with a name", () => {
-    render(<Greet name="Ancilla" />)
-
-    const greetingText = screen.getByText('Hello Ancilla')
-    expect(greetingText).toBeInTheDocument()
-})
+} )
