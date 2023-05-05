a19d0947105f1a8b2d843e922b654640b9d47c0b
numoonchld <raghavendra.saralaya@gmail.com>
2023-05-05 16:15:32 +0530

    providers and hooks testing

--- src/components/mui-mode/MuiMode.test.tsx
+++ src/components/mui-mode/MuiMode.test.tsx
@@ -1,13 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../utils/test-utills'
 import { MuiMode } from './MuiMode'
 import { AppProviders } from '../../providers/AppProviders'
 
 
 describe('MuiMode', () => {
     test('renders text correctly', () => {
-        render(<MuiMode />, {
-            wrapper: AppProviders,
-        })
+        render(<MuiMode />)
 
         const headingElement = screen.getByRole('heading')
         expect(headingElement).toHaveTextContent('dark mode')
